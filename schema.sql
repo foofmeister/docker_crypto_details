f0ad3f511eb578364dd942db05cd46580764cd14
foofmeister <forest.summers@gmail.com>
2022-10-21 22:20:28 -0700

    added mysql docker file

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS `DB`;
+CREATE DATABASE `DB`;
+USE `DB`;
+
+
+DROP TABLE IF EXISTS `ARTIST`;
+CREATE TABLE `Dummy` (
+  `Dummy_column1` datetime,
+  `Dummy_column2` varchar(100) NOT NULL
+);
+
