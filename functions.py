b0af09d300bd8ac339f2e6700b61240a144dd12e
foofmeister <forest.summers@gmail.com>
2022-10-22 19:45:49 -0700

    added files

---
+++ functions.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+connection = create_connection("/home/forest_summers/crypto_stuff/database.db")
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+);
+"""
+
+execute_query(connection, create_users_table)
+
+print("forest")
+
+
